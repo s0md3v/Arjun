3c11b5cd3be064cf4ec170077e67978103e7ce98
Somdev Sangwan <s0md3v@gmail.com>
2019-10-23 12:59:47 +0530

    1.6 build

--- arjun.py
+++ arjun.py
@@ -2,56 +2,58 @@
 
 from __future__ import print_function
 
-from core.colors import red, green, white, end, info, bad, good, run
+from core.colors import green, white, end, info, bad, good, run
 
 print('''%s    _         
    /_| _ '    
-  (  |/ /(//) %sv1.5%s
+  (  |/ /(//) %sv1.6%s
       _/      %s
 ''' % (green, white, green, end))
 
 try:
     import concurrent.futures
 except ImportError:
-    print ('%s Please use Python > 3.2 to run Arjun.' % bad)
+    print('%s Please use Python > 3.2 to run Arjun.' % bad)
     quit()
 
 import re
-import sys
 import json
-import requests
+import time
 import argparse
-
-from urllib.parse import unquote
 
 import core.config
 from core.prompt import prompt
 from core.requester import requester
-from core.utils import e, d, stabilize, randomString, slicer, joiner, unityExtracter, getParams, flattenParams, removeTags, extractHeaders, log
+from core.utils import e, d, stabilize, randomString, slicer, joiner, unityExtracter, getParams, removeTags, extractHeaders
 
-parser = argparse.ArgumentParser() #defines the parser
-#Arguements that can be supplied
+parser = argparse.ArgumentParser() # defines the parser
+# Arguments that can be supplied
 parser.add_argument('-u', help='target url', dest='url')
-parser.add_argument('-f', help='wordlist path', dest='wordlist')
-parser.add_argument('-d', help='request delay', dest='delay', type=int)
-parser.add_argument('-t', help='number of threads', dest='threads', type=int)
 parser.add_argument('-o', help='path for the output file', dest='output_file')
-parser.add_argument('--urls', help='file containing urls', dest='url_file')
+parser.add_argument('-d', help='request delay', dest='delay', type=int, default=0)
+parser.add_argument('-t', help='number of threads', dest='threads', type=int, default=2)
+parser.add_argument('-f', help='wordlist path', dest='wordlist', default='./db/params.txt')
+parser.add_argument('--urls', help='file containing target urls', dest='url_file')
 parser.add_argument('--get', help='use get method', dest='GET', action='store_true')
 parser.add_argument('--post', help='use post method', dest='POST', action='store_true')
-parser.add_argument('--include', help='include this data in every request', dest='include')
 parser.add_argument('--headers', help='add headers', dest='headers', nargs='?', const=True)
 parser.add_argument('--json', help='treat post data as json', dest='jsonData', action='store_true')
-args = parser.parse_args() #arguments to be parsed
+parser.add_argument('--stable', help='prefer stability over speed', dest='stable', action='store_true')
+parser.add_argument('--include', help='include this data in every request', dest='include', default={})
+args = parser.parse_args() # arguments to be parsed
 
 url = args.url
-jsonData = args.jsonData
+delay = args.delay
+stable = args.stable
+include = args.include
 headers = args.headers
-delay = args.delay or 0
+jsonData = args.jsonData
 url_file = args.url_file
-include = args.include or {}
-threadCount = args.threads or 2
-wordlist = args.wordlist or './db/params.txt'
+wordlist = args.wordlist
+threadCount = args.threads
+
+if stable or delay:
+    threadCount = 1
 
 core.config.globalVariables = vars(args)
 
@@ -60,12 +62,17 @@ if type(headers) == bool:
 elif type(headers) == str:
     headers = extractHeaders(headers)
 else:
-    headers = {}
+    headers = {'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0',
+                'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                'Accept-Language' : 'en-US,en;q=0.5',
+                'Accept-Encoding' : 'gzip, deflate',
+                'Connection' : 'keep-alive',
+                'Upgrade-Insecure-Requests' : '1'}
 
 if jsonData:
     headers['Content-type'] = 'application/json'
 
-if args.GET:
+if not (args.GET or args.POST or args.jsonData) or args.GET:
     GET = True
 else:
     GET = False
@@ -78,7 +85,7 @@ try:
         for line in file:
             paramList.append(line.strip('\n'))
 except FileNotFoundError:
-    log('%s The specified file for parameters doesn\'t exist' % bad)
+    print('%s The specified file for parameters doesn\'t exist' % bad)
     quit()
 
 urls = []
@@ -89,11 +96,11 @@ if url_file:
             for line in file:
                 urls.append(line.strip('\n'))
     except FileNotFoundError:
-        log('%s The specified file for URLs doesn\'t exist' % bad)
+        print('%s The specified file for URLs doesn\'t exist' % bad)
         quit()
 
 if not url and not url_file:
-    log('%s No URL specified.' % bad)
+    print('%s No URL specified.' % bad)
     quit()
 
 def heuristic(response, paramList):
@@ -105,23 +112,33 @@ def heuristic(response, paramList):
         for inp in inputs:
             inpName = re.search(r'(?i)name=[\'"](.*?)[\'"]', inp)
             if inpName:
-                inpType = re.search(r'(?i)type=[\'"](.*?)[\'"]', inp)
-                inpValue = re.search(r'(?i)value=[\'"](.*?)[\'"]', inp)
                 inpName = d(e(inpName.group(1)))
                 if inpName not in done:
                     if inpName in paramList:
                         paramList.remove(inpName)
                     done.append(inpName)
                     paramList.insert(0, inpName)
-                    log('%s Heuristic found a potential parameter: %s%s%s' % (good, green, inpName, end))
-                    log('%s Prioritizing it' % good)
+                    print('%s Heuristic found a potential %s parameter: %s%s%s' % (good, method.group(1), green, inpName, end))
+                    print('%s Prioritizing it' % info)
+    emptyJSvars = re.finditer(r'var\s+([^=]+)\s*=\s*[\'"`][\'"`]', response)
+    for each in emptyJSvars:
+        inpName = each.group(1)
+        done.append(inpName)
+        paramList.insert(0, inpName)
+        print('%s Heuristic found a potential parameter: %s%s%s' % (good, green, inpName, end))
+        print('%s Prioritizing it' % info)
 
 def quickBruter(params, originalResponse, originalCode, reflections, factors, include, delay, headers, url, GET):
     joined = joiner(params, include)
     newResponse = requester(url, joined, headers, GET, delay)
     if newResponse.status_code == 429:
-        print ('%s Target has rate limiting in place, please use -t 2 -d 5.' % bad)
-        raise ConnectionError
+        if core.config.globalVariables['stable']:
+            print('%s Hit rate limit, stabilizing the connection..')
+            time.sleep(30)
+            return params
+        else:
+            print('%s Target has rate limiting in place, please use --stable switch' % bad)
+            raise ConnectionError
     if newResponse.status_code != originalCode:
         return params
     elif factors['sameHTML'] and len(newResponse.text) != (len(originalResponse)):
@@ -135,26 +152,6 @@ def quickBruter(params, originalResponse, originalCode, reflections, factors, in
     else:
         return False
 
-def bruter(param, originalResponse, originalCode, factors, include, reflections, delay, headers, url, GET): 
-    fuzz = randomString(6)
-    data = {param : fuzz}
-    data.update(include)
-    response = requester(url, data, headers, GET, delay)
-    newReflections = response.text.count(fuzz)
-    reason = False
-    if response.status_code != originalCode:
-        reason = 'Different response code'
-    elif reflections != newReflections:
-        reason = 'Different number of reflections'
-    elif factors['sameHTML'] and len(response.text) != (len(originalResponse)):
-        reason = 'Different content length'
-    elif factors['samePlainText'] and len(removeTags(response.text)) != (len(removeTags(originalResponse))):
-        reason = 'Different plain-text content length'
-    if reason:
-        return {param : reason}
-    else:
-        return None
-
 def narrower(oldParamList, url, include, headers, GET, delay, originalResponse, originalCode, reflections, factors, threadCount):
     newParamList = []
     threadpool = concurrent.futures.ThreadPoolExecutor(max_workers=threadCount)
@@ -162,107 +159,101 @@ def narrower(oldParamList, url, include, headers, GET, delay, originalResponse, 
     for i, result in enumerate(concurrent.futures.as_completed(futures)):
         if result.result():
             newParamList.extend(slicer(result.result()))
-        log('%s Processing: %i/%-6i' % (info, i + 1, len(oldParamList)), mode='run')
+        print('%s Processing: %i/%-6i' % (info, i + 1, len(oldParamList)), end='\r')
     return newParamList
 
 def initialize(url, include, headers, GET, delay, paramList, threadCount):
     url = stabilize(url)
+    if not url:
+        return {}
+    else:
+        print('%s Analysing the content of the webpage' % run)
+        firstResponse = requester(url, include, headers, GET, delay)
 
-    log('%s Analysing the content of the webpage' % run)
-    firstResponse = requester(url, include, headers, GET, delay)
+        print('%s Analysing behaviour for a non-existent parameter' % run)
 
-    log('%s Analysing behaviour for a non-existent parameter' % run)
+        originalFuzz = randomString(6)
+        data = {originalFuzz : originalFuzz[::-1]}
+        data.update(include)
+        response = requester(url, data, headers, GET, delay)
+        reflections = response.text.count(originalFuzz[::-1])
+        print('%s Reflections: %s%i%s' % (info, green, reflections, end))
 
-    originalFuzz = randomString(6)
-    data = {originalFuzz : originalFuzz[::-1]}
-    data.update(include)
-    response = requester(url, data, headers, GET, delay)
-    reflections = response.text.count(originalFuzz[::-1])
-    log('%s Reflections: %s%i%s' % (info, green, reflections, end))
+        originalResponse = response.text
+        originalCode = response.status_code
+        print('%s Response Code: %s%i%s' % (info, green, originalCode, end))
 
-    originalResponse = response.text
-    originalCode = response.status_code
-    log('%s Response Code: %s%i%s' % (info, green, originalCode, end))
+        newLength = len(response.text)
+        plainText = removeTags(originalResponse)
+        plainTextLength = len(plainText)
+        print('%s Content Length: %s%i%s' % (info, green, newLength, end))
+        print('%s Plain-text Length: %s%i%s' % (info, green, plainTextLength, end))
 
-    newLength = len(response.text)
-    plainText = removeTags(originalResponse)
-    plainTextLength = len(plainText)
-    log('%s Content Length: %s%i%s' % (info, green, newLength, end))
-    log('%s Plain-text Length: %s%i%s' % (info, green, plainTextLength, end))
+        factors = {'sameHTML': False, 'samePlainText': False}
+        if len(firstResponse.text) == len(originalResponse):
+            factors['sameHTML'] = True
+        elif len(removeTags(firstResponse.text)) == len(plainText):
+            factors['samePlainText'] = True
 
-    factors = {'sameHTML': False, 'samePlainText': False}
-    if len(firstResponse.text) == len(originalResponse):
-        factors['sameHTML'] = True
-    elif len(removeTags(firstResponse.text)) == len(plainText):
-        factors['samePlainText'] = True
+        print('%s Parsing webpage for potential parameters' % run)
+        heuristic(firstResponse.text, paramList)
 
-    log('%s Parsing webpage for potential parameters' % run)
-    heuristic(firstResponse.text, paramList)
+        fuzz = randomString(8)
+        data = {fuzz : fuzz[::-1]}
+        data.update(include)
 
-    fuzz = randomString(8)
-    data = {fuzz : fuzz[::-1]}
-    data.update(include)
+        print('%s Performing heuristic level checks' % run)
 
-    log('%s Performing heuristic level checks' % run)
-
-    toBeChecked = slicer(paramList, 50)
-    foundParams = []
-    while True:
-        try:
+        toBeChecked = slicer(paramList, 50)
+        foundParamsTemp = []
+        while True:
             toBeChecked = narrower(toBeChecked, url, include, headers, GET, delay, originalResponse, originalCode, reflections, factors, threadCount)
-            toBeChecked = unityExtracter(toBeChecked, foundParams)
+            toBeChecked = unityExtracter(toBeChecked, foundParamsTemp)
             if not toBeChecked:
                 break
-        except:
-            raise ConnectionError
 
-    if foundParams:
-        log('%s Heuristic found %i potential parameters.' % (info, len(foundParams)))
-        paramList = foundParams
+        foundParams = []
 
-    currentResult = []
-    returnResult = []
+        for param in foundParamsTemp:
+            exists = quickBruter([param], originalResponse, originalCode, reflections, factors, include, delay, headers, url, GET)
+            if exists:
+                foundParams.append(param)
 
-    threadpool = concurrent.futures.ThreadPoolExecutor(max_workers=threadCount)
-    futures = (threadpool.submit(bruter, param, originalResponse, originalCode, factors, include, reflections, delay, headers, url, GET) for param in foundParams)
-    for i, result in enumerate(concurrent.futures.as_completed(futures)):
-        if result.result():
-            currentResult.append(result.result())
-        log('%s Progress: %i/%i' % (info, i + 1, len(paramList)), mode='run')
+        print('%s Scan Completed    ' % info)
 
-    log('%s Scan Completed    ' % info)
-
-    for each in currentResult:
-        for param, reason in each.items():
-            log('%s Valid parameter found: %s%s%s' % (good, green, param, end))
-            log('%s Reason: %s' % (info, reason))
-            returnResult.append({"param": param, "reason": reason})
-    if not returnResult:
-        log('%s Unable to verify existence of parameters detected by heuristic' % bad)
-    return returnResult
+        for each in foundParams:
+            print('%s Valid parameter found: %s%s%s' % (good, green, each, end))
+        if not foundParams:
+            print('%s Unable to verify existence of parameters detected by heuristic.' % bad)
+        return foundParams
 
 finalResult = {}
-if url:
-    finalResult[url] = []
-    try:
-        finalResult[url] = initialize(url, include, headers, GET, delay, paramList, threadCount)
-    except ConnectionError:
-        print ('%s Target is refusing connections. Consider using -d 5 -t 1.' % bad)
-        quit()
-elif urls:
-    for url in urls:
+
+try:
+    if url:
         finalResult[url] = []
-        print('%s Scanning: %s' % (run, url))
         try:
-            finalResult[url] = initialize(url, include, headers, GET, delay, list(paramList), threadCount)
-            if finalResult[url]:
-                print('%s Parameters found: %s' % (good, ', '.join([each['param'] for each in finalResult[url]])))
+            finalResult[url] = initialize(url, include, headers, GET, delay, paramList, threadCount)
         except ConnectionError:
-            print ('%s Target is refusing connections. Consider using -d 5 -t 1.' % bad)
-            pass
+            print('%s Target has rate limiting in place, please use --stable switch.' % bad)
+            quit()
+    elif urls:
+        for url in urls:
+            finalResult[url] = []
+            print('%s Scanning: %s' % (run, url))
+            try:
+                finalResult[url] = initialize(url, include, headers, GET, delay, list(paramList), threadCount)
+                if finalResult[url]:
+                    print('%s Parameters found: %s' % (good, ', '.join(finalResult[url])))
+            except ConnectionError:
+                print('%s Target has rate limiting in place, please use --stable switch.' % bad)
+                pass
+except KeyboardInterrupt:
+    print('%s Exiting..                ' % bad)
+    quit()
 
 # Finally, export to json
 if args.output_file and finalResult:
-    log('%s Saving output to JSON file in %s' % (info, args.output_file))
+    print('%s Saving output to JSON file in %s' % (info, args.output_file))
     with open(str(args.output_file), 'w+') as json_output:
         json.dump(finalResult, json_output, sort_keys=True, indent=4)
--- core/requester.py
+++ core/requester.py
@@ -1,3 +1,4 @@
+import re
 import json
 import time
 import random
@@ -11,13 +12,10 @@ warnings.filterwarnings('ignore') # Disable SSL related warnings
 def requester(url, data, headers, GET, delay):
     if core.config.globalVariables['jsonData']:
         data = json.dumps(data)
+    if core.config.globalVariables['stable']:
+        delay = random.choice(range(6, 12))
     time.sleep(delay)
-    user_agents = ['Mozilla/5.0 (X11; Linux i686; rv:60.0) Gecko/20100101 Firefox/60.0',
-'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
-'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36 OPR/43.0.2442.991']
-    if headers:
-    	if 'User-Agent' not in headers:
-    		headers['User-Agent'] = random.choice(user_agents)
+    headers['Host'] = re.search(r'https?://([^/]+)', url).group(1)
     if GET:
         response = requests.get(url, params=data, headers=headers, verify=False)
     elif core.config.globalVariables['jsonData']:
