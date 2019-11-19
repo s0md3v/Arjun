ce3f7fc463973743f908da053e850ffa159fbe2a
Somdev Sangwan <s0md3v@gmail.com>
2019-11-19 19:00:56 +0530

    updated code to 1.5

--- arjun/core/bruteforcer.py
+++ arjun/core/bruteforcer.py
@@ -1,4 +1,7 @@
 """The brute forcer of Arjun."""
+import time
+
+import arjun.core.config
 from arjun.core.requester import requester
 from arjun.core.utils import joiner, random_string, remove_tags
 
@@ -13,68 +16,28 @@ def quick_brute_force(
     delay,
     headers,
     url,
-    get,
-    variables,
+    get
 ):
     """Quick brute forcer."""
-    merged_parameters = joiner(params, include)
-    new_response = requester(
-        url, merged_parameters, headers, get, delay, variables
-    )
+    joined = joiner(params, include)
+    new_response = requester(url, joined, headers, get, delay)
+    if new_response.status_code == 429:
+        if core.config.global_vars['stable']:
+            print('%s Hit rate limit, stabilizing the connection..')
+            time.sleep(30)
+            return params
+        else:
+            print('%s Target has rate limiting in place, please use --stable switch' % bad)
+            raise ConnectionError
     if new_response.status_code != original_code:
         return params
-    elif factors['same_html'] and len(new_response.text) != (
-        len(original_response)
-    ):
+    elif factors['same_html'] and len(new_response.text) != (len(original_response)):
         return params
-    elif factors['same_plain_text'] and len(
-        remove_tags(original_response)
-    ) != len(remove_tags(new_response.text)):
+    elif factors['same_plain_text'] and len(remove_tags(original_response)) != len(remove_tags(new_response.text)):
         return params
     elif True:
-        for param, value in merged_parameters.items():
-            if (
-                param not in include
-                and new_response.text.count(value) != reflections
-            ):
+        for param, value in joined.items():
+            if param not in include and new_response.text.count(value) != reflections:
                 return params
     else:
         return False
-
-
-def brute_force(
-    param,
-    original_response,
-    original_code,
-    factors,
-    include,
-    reflections,
-    delay,
-    headers,
-    url,
-    get,
-    variables,
-):
-    """Standard brute forcer."""
-    fuzz = random_string(6)
-    data = {param: fuzz}
-    data.update(include)
-    response = requester(url, data, headers, get, delay, variables)
-    new_reflections = response.text.count(fuzz)
-    reason = False
-    if response.status_code != original_code:
-        reason = 'Different response code'
-    elif reflections != new_reflections:
-        reason = 'Different number of reflections'
-    elif factors['same_html'] and len(response.text) != (
-        len(original_response)
-    ):
-        reason = 'Different content length'
-    elif factors['same_plain_text'] and len(remove_tags(response.text)) != (
-        len(remove_tags(original_response))
-    ):
-        reason = 'Different plain-text content length'
-    if reason:
-        return {param: reason}
-    else:
-        return None
