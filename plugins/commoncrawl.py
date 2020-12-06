8db42e06b27695179d6423b1801cd57b77664999
Somdev Sangwan <s0md3v@gmail.com>
2020-12-06 15:03:58 +0530

    added plugins for passive parameter collection

---
+++ plugins/commoncrawl.py
@@ -0,0 +1,14 @@
+import requests
+
+from urllib.parse import urlparse
+
+def commoncrawl(host, page=0):
+	these_params = set()
+	response = requests.get('http://index.commoncrawl.org/CC-MAIN-2020-29-index?url=*.%s&fl=url&page=%s&limit=10000' % (host, page)).text
+	if response.startswith('<!DOCTYPE html>'):
+		return ([], False, 'commoncrawl')
+	urls = response.split('\n')
+	for url in urls:
+		for param in urlparse(url).query.split('&'):
+			these_params.add(param.split('=')[0])
+	return (these_params, True, 'commoncrawl')
